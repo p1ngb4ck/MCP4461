fee7576d42437c0cada27e4657fa3e834244c344
Oliver Kleinecke <okleinecke@web.de>
2025-01-21 01:02:58 +0100

    Update MCP4461.h
    full overhaul of library

--- MCP4461.h
+++ MCP4461.h
@@ -9,23 +9,15 @@
 //For all the Wipers 0x100 = Full scale, 0x80 = mid scale, 0x0 = Zero scale
 #define MCP4461_VW0 0x00
 #define MCP4461_VW1 0x10
-#define MCP4461_NVW0 0x20
-#define MCP4461_NVW1 0x30
-//TCON0: D8: Reserved D7:R1HW D6: R1A D5:R1W D4:R1B D3:R0HW D2:R0A D1:R0W D0: R0B
-#define MCP4461_TCON0 0x40
-//STATUS: D8:D7: Reserved D6: WL3 D5:WL2 D4:EEWA D3:WL1 D2:WL0 D1:Reserved D0: WP
-#define MCP4461_STATUS 0x50
 #define MCP4461_VW2 0x60
 #define MCP4461_VW3 0x70
-#define MCP4461_NVW2 0x80
-#define MCP4461_NVW3 0x90
-//TCON0: D8: Reserved D7:R3HW D6: R3A D5:R3W D4:R3B D3:R2HW D2:R2A D1:R2W D0: R2B
+//STATUS: D8:D7: Reserved D6: WL3 D5:WL2 D4:EEWA D3:WL1 D2:WL0 D1:Reserved D0: WP
+#define MCP4461_STATUS 0x50
+//TCON0: D8: Reserved D7:R1HW D6: R1A D5:R1W D4:R1B D3:R0HW D2:R0A D1:R0W D0: R0B
+#define MCP4461_TCON0 0x40
+//TCON1: D8: Reserved D7:R3HW D6: R3A D5:R3W D4:R3B D3:R2HW D2:R2A D1:R2W D0: R2B
 #define MCP4461_TCON1 0xA0
 #define MCP4461_EEPROM_DATA_ADDRESS_1 0xB0
-#define MCP4461_EEPROM_DATA_ADDRESS_2 0xC0
-#define MCP4461_EEPROM_DATA_ADDRESS_3 0xD0
-#define MCP4461_EEPROM_DATA_ADDRESS_4 0xE0
-#define MCP4461_EEPROM_DATA_ADDRESS_5 0xF0
 
 //control commands
 #define MCP4461_WRITE 0x0
@@ -37,23 +29,23 @@ class MCP4461{
 public:
   MCP4461(uint8_t);
   void setMCP4461Address(uint8_t);
-  void setVolatileWiper(uint8_t, uint16_t);
-  void setNonVolatileWiper(uint8_t, uint16_t);
-  void setVolatileWipers(uint16_t);
-  void setNonVolatileWipers(uint16_t);
+  uint16_t readAddress(uint8_t);
+  void writeValue(uint8_t, uint16_t);
+  uint16_t getWiper(uint8_t, bool);
+  void setWiper(uint8_t, uint16_t, bool);
+  void setWipers(uint16_t, bool);
+  uint8_t getStatus();
+  bool getEEPRomWriteActive();
   uint8_t getTerminalRegister(uint8_t);
   void setTerminalRegister(uint8_t, uint8_t);
-  bool getTerminalState(uint8_t, char);
+  uint8_t getTerminalState(uint8_t, char);
+  uint8_t getTerminalStateBitNumber(uint8_t, char);
   void setTerminalState(uint8_t, char, uint8_t);
   void connectWiper(uint8_t);
   void disconnectWiper(uint8_t);
   void toggleWiper(uint8_t);
-  uint16_t getVolatileWiper(uint8_t);
-  uint16_t getNonVolatileWiper(uint8_t) const;
   uint16_t getEEPRomGeneralPurposeData(uint8_t);
   void setEEPRomGeneralPurposeData(uint8_t, uint16_t);
-  uint8_t getStatus();
-  bool getEEPRomWriteActive();
 
 private:
   uint8_t _mcp4461_address;
